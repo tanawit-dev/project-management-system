49191c7e1586852d0d6d74b2d73538302985d5b5
tanawit.dev <tanawit.dev@hotmail.com>
2021-01-04 11:09:36 +0700

    Improve UI by using bootstrap

--- src/main/java/com/example/projectmanagementsystem/model/Project.java
+++ src/main/java/com/example/projectmanagementsystem/model/Project.java
@@ -9,9 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Entity
