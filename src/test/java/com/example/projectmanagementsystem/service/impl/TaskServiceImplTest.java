3ed97a9c2b6f80aef9d1d6188cc9d68a8a7b0df1
tanawit.dev <tanawit.dev@hotmail.com>
2021-02-12 14:18:50 +0700

    Rewrite test

--- src/test/java/com/example/projectmanagementsystem/service/impl/TaskServiceImplTest.java
+++ src/test/java/com/example/projectmanagementsystem/service/impl/TaskServiceImplTest.java
@@ -3,7 +3,6 @@ package com.example.projectmanagementsystem.service.impl;
 import com.example.projectmanagementsystem.model.Task;
 import com.example.projectmanagementsystem.repository.TaskRepository;
 import com.example.projectmanagementsystem.service.ITaskService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,11 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,86 +28,114 @@ public class TaskServiceImplTest {
     @Autowired
     TaskRepository taskRepository;
 
-    private final int INIT_SIZE = 3;
-
     @BeforeEach
-    public void beforeEach() {
-        for (int i = 1; i <= INIT_SIZE; i++) {
-            Task task = new Task();
-            task.setName("task " + i);
-            taskRepository.save(task);
-        }
-    }
-
-    @AfterEach
-    public void afterEach() {
+    public void cleanup() {
         taskRepository.deleteAll();
     }
 
     @Test
     @DisplayName("Should find all task then success")
     public void shouldFindAll_thenSuccess() {
+        persistToDatabase(
+                new Task("task 1"),
+                new Task("task 2"),
+                new Task("task 3")
+        );
+
         List<Task> tasks = taskService.findAll();
-        assertNotNull(tasks);
-        assertEquals(INIT_SIZE, tasks.size());
+
+        assertThat(tasks)
+                .hasSize(3)
+                .extracting("name")
+                .containsExactly("task 1", "task 2", "task 3");
     }
 
     @Test
     @DisplayName("Should find by task id then success")
     public void shouldFindByTaskId_thenSuccess() {
-        String name = "task";
-        String detail = "detail";
+        String name = "task 1";
+        String detail = "task detail";
         LocalDateTime beginAt = LocalDateTime.now();
         LocalDateTime finishAt = beginAt.plusDays(1);
-        Task task = new Task();
-        task.setName(name);
-        task.setDetail(detail);
-        task.setBeginAt(beginAt);
-        task.setFinishAt(finishAt);
+        Task expectedTask = new Task(name, beginAt, finishAt, detail);
 
-        Long id = taskService.create(task).getId();
-        Optional<Task> taskOptional = taskService.findById(id);
+        Long id = taskService.create(expectedTask).getId();
+        Optional<Task> actuallyTask = taskService.findById(id);
 
-        assertTrue(taskOptional.isPresent());
-        task = taskOptional.get();
+        assertThat(actuallyTask.isPresent()).isTrue();
+        assertThat(actuallyTask.get().getId()).isNotNull().isNotZero();
+        assertThat(actuallyTask.get().getName()).isEqualTo(expectedTask.getName());
+        assertThat(actuallyTask.get().getDetail()).isEqualTo(expectedTask.getDetail());
+        assertThat(actuallyTask.get().getBeginAt()).isEqualToIgnoringSeconds(expectedTask.getBeginAt());
+        assertThat(actuallyTask.get().getFinishAt()).isEqualToIgnoringSeconds(expectedTask.getFinishAt());
+    }
 
-        assertEquals(name, task.getName());
-        assertEquals(detail, task.getDetail());
-        assertThat(task.getBeginAt()).isEqualToIgnoringSeconds(beginAt);
-        assertThat(task.getFinishAt()).isEqualToIgnoringSeconds(finishAt);
+    @Test
+    @DisplayName("Should not found any task when given non existing id")
+    public void shouldNotFound_whenGivenNonExistingId() {
+        Optional<Task> nonExistingTask = taskService.findById(new Random().nextLong());
+
+        assertThat(nonExistingTask.isPresent()).isFalse();
     }
 
     @Test
     @DisplayName("Should create task then success")
     public void shouldCreateTask_thenSuccess() {
-        int count = taskService.findAll().size();
-        Task task = new Task();
-        task.setName("task");
+        String name = "task 1";
+        String detail = "task detail";
+        LocalDateTime beginAt = LocalDateTime.now();
+        LocalDateTime finishAt = beginAt.plusDays(1);
+        Task expectedTask = new Task(name, beginAt, finishAt, detail);
 
-        Task createdTask = taskService.create(task);
+        Task actuallyTask = taskService.create(expectedTask);
 
-        assertEquals(count + 1, taskService.findAll().size());
-        assertNotNull(createdTask);
-        assertNotNull(createdTask.getId());
+        assertThat(actuallyTask).isNotNull();
+        assertThat(actuallyTask.getId()).isNotNull().isNotZero();
+        assertThat(actuallyTask.getName()).isEqualTo(expectedTask.getName());
+        assertThat(actuallyTask.getDetail()).isEqualTo(expectedTask.getDetail());
+        assertThat(actuallyTask.getBeginAt()).isEqualToIgnoringSeconds(expectedTask.getBeginAt());
+        assertThat(actuallyTask.getFinishAt()).isEqualToIgnoringSeconds(expectedTask.getFinishAt());
+        assertThat(actuallyTask.getCreateAt()).isBefore(LocalDateTime.now()).isAfter(LocalDateTime.now().minusMinutes(5));
     }
 
     @Test
     @DisplayName("Should update task then success")
     public void shouldUpdateTask_thenSuccess() {
-        Task task = taskService.findAll().get(0);
-        String name = "task edit";
-        String detail = "detail edit";
+        String name = "task 1";
+        String detail = "task detail";
         LocalDateTime beginAt = LocalDateTime.now();
         LocalDateTime finishAt = beginAt.plusDays(1);
+        Task expectedTask = new Task(name, beginAt, finishAt, detail);
 
-        task.setName(name);
-        task.setDetail(detail);
-        task.setBeginAt(beginAt);
-        task.setFinishAt(finishAt);
+        expectedTask = taskService.create(expectedTask);
+        expectedTask.setName(name + " edit");
+        expectedTask.setDetail(detail + " edit");
+        expectedTask.setBeginAt(LocalDateTime.now().plusDays(5));
+        expectedTask.setFinishAt(expectedTask.getBeginAt().plusDays(1));
+        Task actuallyTask = taskService.update(expectedTask.getId(), expectedTask);
 
-        taskService.update(task.getId(), task);
-        Optional<Task> retrivedTaskOpt = taskService.findById(task.getId());
-        assertTrue(retrivedTaskOpt.isPresent());
-        Task retrivedTask = retrivedTaskOpt.get();
+        assertThat(actuallyTask).isNotNull();
+        assertThat(actuallyTask.getId()).isEqualTo(expectedTask.getId());
+        assertThat(actuallyTask.getName()).isEqualTo(expectedTask.getName());
+        assertThat(actuallyTask.getDetail()).isEqualTo(expectedTask.getDetail());
+        assertThat(actuallyTask.getBeginAt()).isEqualToIgnoringSeconds(expectedTask.getBeginAt());
+        assertThat(actuallyTask.getFinishAt()).isEqualToIgnoringSeconds(expectedTask.getFinishAt());
+    }
+
+    @Test
+    @DisplayName("Should delete task success")
+    public void shouldDeleteTask_thenSuccess() {
+        Task expectedTask = new Task("task 1");
+
+        expectedTask = taskService.create(expectedTask);
+        taskService.delete(expectedTask.getId());
+        Optional<Task> actuallyTask = taskService.findById(expectedTask.getId());
+
+        assertThat(actuallyTask.isPresent()).isFalse();
+        assertThat(taskRepository.count()).isZero();
+    }
+
+    private void persistToDatabase(Task... tasks) {
+        taskRepository.saveAll(Arrays.asList(tasks));
     }
 }
